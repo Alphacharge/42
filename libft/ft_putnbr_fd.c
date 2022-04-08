cc957ece81620f176edccd9e232472f7ca92f227
Rouven Betz <rbetz@1-B-16.42heilbronn.de>
2022-04-08 18:38:39 +0200

    putnbr changed

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: rbetz <rbetz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/08 16:26:24 by rbetz             #+#    #+#             */
-/*   Updated: 2022/04/08 17:30:29 by rbetz            ###   ########.fr       */
+/*   Updated: 2022/04/08 17:47:30 by rbetz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 void	ft_putnbr_fd(int n, int fd)
 {
 	if (n == -2147483648)
-    {
-        ft_putchar_fd('-', fd);
-        ft_putchar_fd('2', fd);
-        ft_putnbr_fd(147483648, fd);
-    }
+	{
+		ft_putchar_fd('-', fd);
+		ft_putchar_fd('2', fd);
+		ft_putnbr_fd(147483648, fd);
+	}
 	else if (n > 9)
 	{
 		ft_putnbr_fd(n / 10, fd);
